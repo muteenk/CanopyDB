199cde84183680914754c9443a798aa25479c0c0
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-05-23 02:18:50 +0530

    Comment cleanup

--- src/main/java/org/canopydb/models/TableData.java
+++ src/main/java/org/canopydb/models/TableData.java
@@ -7,17 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TableData {
-//    private final String tableName;
-//    private final String databaseName;
     private final List<String> headers = new ArrayList<>();
     private final List<List<String>> rows = new ArrayList<>();
-//    private final TableQuery tableQuery;
-
-//    public TableData(String table, String database, TableQuery query) {
-//        tableName = table;
-//        databaseName = database;
-//        tableQuery = query;
-//    }
 
     public void appendHeader(String col){
         this.headers.add(col);
@@ -34,12 +25,4 @@ public class TableData {
     public List<List<String>> getRows() {
         return this.rows;
     }
-
-//    public String getTableName() {return this.tableName;}
-//
-//    public String getDatabaseName() {return this.databaseName;}
-//
-//    public String getTablePath() {return TableUtilities.tablePath(this.databaseName, this.tableName);}
-//
-//    public TableQuery getTableQuery() {return this.tableQuery;}
 }

d0349a13ac3e4e989da9c4bca932d6291f87a2c0
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-06-12 18:41:11 +0530

    Added: Basic event listening logic for filter options

--- src/main/java/org/canopydb/ui/molecules/TabFilterArea.java
+++ src/main/java/org/canopydb/ui/molecules/TabFilterArea.java
@@ -2,17 +2,16 @@ package org.canopydb.ui.molecules;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import org.canopydb.controllers.TableViewEventController;
 import org.canopydb.models.TableSession;
 import org.canopydb.ui.atoms.TextInput;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class TabFilterArea {
     private final VBox filterArea = new VBox();
@@ -29,6 +28,7 @@ public class TabFilterArea {
         clearAllFilters.addEventHandler(MouseEvent.MOUSE_CLICKED, _ -> {
             clearAllFilterInputs();
         });
+        clearAllFilters.setDisable(true);
 
         HBox controls = new HBox(
                 addNewFilter,
@@ -52,6 +52,15 @@ public class TabFilterArea {
         filterBox.getStyleClass().add("filter-row");
         HBox.setHgrow(filterInput, Priority.ALWAYS);
 
+        filterInput.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                event.consume();
+                filterToggle.setText("Applied");
+            }
+        });
+        filterToggle.addEventHandler(MouseEvent.MOUSE_CLICKED, _ -> {
+            filterToggle.setText("Applied");
+        });
         removeFilter.addEventHandler(MouseEvent.MOUSE_CLICKED, _ -> {
             removeFilterInput(filterBox);
         });
@@ -64,16 +73,19 @@ public class TabFilterArea {
         if (currentSize > MAX_INPUT_COUNT) return;
         filterArea.getChildren().addFirst(buildFilterInput());
         this.addNewFilter.setDisable(currentSize == MAX_INPUT_COUNT);
+        this.clearAllFilters.setDisable(false);
     }
 
     private void removeFilterInput(HBox filterBox){
         filterArea.getChildren().remove(filterBox);
         this.addNewFilter.setDisable(false);
+        this.clearAllFilters.setDisable(this.filterArea.getChildren().size() == 1);
     }
 
     private void clearAllFilterInputs() {
         filterArea.getChildren().remove(0, this.filterArea.getChildren().size()-1);
         this.addNewFilter.setDisable(false);
+        this.clearAllFilters.setDisable(true);
     }
 
     public VBox getFilterArea() {return this.filterArea;}
